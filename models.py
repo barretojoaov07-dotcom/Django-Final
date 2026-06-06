03076520c221423a563c86dfca1168a9c7739f33
joaoabezerra <gamestooony@outlook.com>
2026-06-06 01:35:41 -0300

    Fix circular import by removing unused imports
    Removed unused imports to prevent circular import issues.

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.urls import reverse
-from products.models import Product
+# removed unused imports that caused circular import
+
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200)
